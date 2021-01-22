f0d5251c9754b5520e0730bb4f3b4105a4201ae8
Supegg <supegg.rao@gmail.com>
2021-01-22 14:47:45 +0800

    remove requests demo's proxy

--- requests/zimuku.py
+++ requests/zimuku.py
@@ -14,6 +14,13 @@ import time
 from bs4 import BeautifulSoup
 
 
+headers = {
+    # 'Referer': f"http://zmk.pw/dld/{number}.html",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36', 
+    'Cookie':'_ga=GA1.2.174161430.1587359254; PHPSESSID=3f786ljte40r2g94v16v644df4; _gid=GA1.2.1593932961.1611276884; zmk_home_view_movieid=51949; zmk_home_view_subid=125044; _gat_gtag_UA_136876477_1=1',
+    'Host': 'www.zimuku.la',
+}
+
 def get_work_names(html):
     """
     获取电影/美剧的中文和英文名称
@@ -55,7 +62,7 @@ def get_dld_url(number):
     :return:
     """
     _url = 'http://zmk.pw/dld/{}.html'.format(number)
-    r = requests.get(_url, timeout=5)
+    r = requests.get(_url, headers=headers, timeout=5)
     soup = BeautifulSoup(r.text, 'lxml')
     random_num = random.randint(0, 3)
     try:
@@ -75,92 +82,24 @@ def get_sub_content(number, url):
     :param url:
     :return:
     """
-    headers = {
-        'Referer': f"http://zmk.pw/dld/{number}.html",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
-    }
-    proxies_list = text_read('proxies.txt')
-    proxies_len = len(proxies_list)
+    headers['Referer'] = f"http://zmk.pw/dld/{number}.html"
 
-    global proxie_num
-    global proxie_temp
-    proxie_temp = proxie_temp+1
-    # 每个ip每次下载的次数
-    if proxie_temp == 5:
-        proxie_num = (proxie_num+1) % proxies_len
-        proxie_temp = 0
+    try:
+        # allow_redirects=False, 301/302重定向资源的问题
+        res = requests.get(url, headers=headers, stream=True,
+                            timeout=5, verify=False, allow_redirects=False)
+        print(res.headers['location'])
 
-    proxies = {'http': 'http://{}'.format(proxies_list[proxie_num])}
-    proxies_states = False
-    while(not proxies_states):
-        try:
-            print(proxies)
-            print('代理ip:', proxie_num)
-            print('此ip使用的次数', proxie_temp)
-            # allow_redirects=False, 301/302重定向资源的问题
-            res = requests.get(url, headers=headers, stream=True,
-                               proxies=proxies, timeout=5, verify=False, allow_redirects=False)
-            print(res.headers['location'])
+        res = requests.get(res.headers['location'], headers=headers, stream=True,
+                            timeout=5, verify=False, allow_redirects=False)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        return None
 
-            res = requests.get(res.headers['location'], headers=headers, stream=True,
-                               proxies=proxies, timeout=5, verify=False, allow_redirects=False)
-
-            # proxie_num=(proxie_num+1)%proxies_len
-            proxies_states = True
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
-            # print('该代理已过期或无法使用')
-            # print('切换代理地址')
-            print('代理ip:', proxie_num)
-            proxie_num = (proxie_num+1) % proxies_len
-            proxie_temp = 0
-            proxies = {'http': 'http://{}'.format(proxies_list[proxie_num])}
-            proxies_states = False
-            return None
-
-    #res = requests.get(url, headers=headers, stream=True,timeout=20,verify=False)
     if res.status_code == 404 and res.history:  # 代理的情况下需要重新用响应的url再请求一遍
-        res = requests.get(res.url)
+        res = requests.get(res.url, headers=headers, allow_redirects=False)
         if res.status_code != 200:
             return None
-
-    return res.content
-
-
-def get_sub_content_bjproxy(number, url):
-    """
-    获取字幕文件
-    :param number:
-    :param url:
-    :return:
-    """
-    headers = {
-        'Referer': "http://zmk.pw/dld/{}.html".format(number),
-    }
-
-    PROXY_POOL_HTTP = 'http://www.bjproxy.com/vps_server/getIpPort.do?orderCode=2018081110395839943749%20&areaCode=0&format=1'
-
-    global proxie_num
-    global proxie_temp
-    proxies = {}
-    # url1='https://www.baidu.com/robots.txt'
-    proxies['http'] = 'http://'+requests.get(PROXY_POOL_HTTP).text
-    print(proxies)
-
-    proxies_states = False
-    while(not proxies_states):
-        try:
-            print(proxies)
-            print('代理ip:', proxie_num)
-            print('此ip使用的次数', proxie_temp)
-            res = requests.get(url, headers=headers, stream=True,
-                               proxies=proxies, timeout=1, verify=False)
-            proxies_states = True
-        except requests.exceptions.ProxyError as e:
-            print('该代理已过期或无法使用')
-            print('切换代理地址')
-            proxies['http'] = 'http://'+requests.get(PROXY_POOL_HTTP).text
-            proxies_states = False
 
     return res.content
 
@@ -185,37 +124,19 @@ def save(filename, content):
         print('[√] file: {}, saved'.format(filename[:30]))
 
 
-def text_read(filename):
-    # Try to read a txt file and return a list. Return [] if there was a mistake.
-    try:
-        file = open(filename, 'r')
-    except IOError:
-        print("Unexpected error:", sys.exc_info()[0])
-        error = []
-        return error
-    content = file.readlines()
-
-    for i in range(len(content)):
-        content[i] = content[i][:len(content[i])-1]
-
-    file.close()
-    return content
-
-
 def main(s, e=1000000):
     start = s
     end = e  # 147269
-    global proxie_num
+    
     for index, url in url_iterator(start, end):
         while True:
-            time.sleep(random.randint(1, 5))  # 友好的爬虫
+            time.sleep(random.randint(3, 5))  # 友好的爬虫
             print('---------------------------------')
             print('at : #', index)
             print(url)
-            # print('代理ip:',proxie_num)
-            # print('代理ip使用个数',proxie_num)
+            
             try:  # 获取字幕详情界面
-                r = requests.get(url, timeout=5, allow_redirects=False)
+                r = requests.get(url, headers=headers, timeout=5, allow_redirects=False)
                 if r.status_code == 302:
                     print('页面已被移除: {}'.format(url))
                     break
@@ -262,7 +183,7 @@ def main(s, e=1000000):
                 if not None:
                     if len(content) < 1024:
                         continue
-                    #content = get_sub_content_bjproxy(number=index, url=_dld_url)
+                    
                     save(sub_name, content)
                     open('start', 'wt').write(str(index))
                     open('sub_list', 'at', encoding='utf-8').write(
@@ -277,11 +198,6 @@ def main(s, e=1000000):
 if __name__ == '__main__':
     logging.basicConfig(
         format="%(levelname)s : %(message)s", level=logging.INFO)
-    global proxie_num
-    global proxie_temp
-    global res
-    proxie_temp = 0
-    proxie_num = 0
 
     s = int(open('start').readline())+1
     print(f"start from {s}")
